b6781a2a3ffe8dd3f222109c286f3aeea1d30b19
EngrMuqeetAhmad <engrmuqeetahmad@gmail.com>
2026-01-03 13:02:19 +0500

    fixed syntax error

--- 02_git/clone_repos.sh
+++ 02_git/clone_repos.sh
@@ -17,19 +17,17 @@ sudo mkdir -p /var/www
 
 sudo chown -R $USER:$USER /var/www
 
-
 clone_repo () {
     GIT_URL=$1
     REPO_NAME=$(basename -s .git "$GIT_URL")
     TARGET="/var/www/$REPO_NAME"
 
-    if [ ! -d "$TARGET"]: then
-      git clone "$GIT_URL" "$TARGET"
+    if [ ! -d "$TARGET" ]; then
+        git clone "$GIT_URL" "$TARGET"
     else
-      cd "$TARGET"
-      git pull
+        cd "$TARGET"
+        git pull
     fi
 }
-
 clone_repo "$FRONTEND_GIT"
 clone_repo "$BACKEND_GIT"
