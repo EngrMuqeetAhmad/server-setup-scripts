00df879ed4791373e41f8eeb14ad5bf322a0d67d
EngrMuqeetAhmad <engrmuqeetahmad@gmail.com>
2026-01-03 16:03:15 +0500

    test

--- 05_nginx/configure_nginx.sh
+++ 05_nginx/configure_nginx.sh
@@ -20,8 +20,17 @@ server {
     root /var/www/$FRONTEND/dist;
     index index.html;
 
-    location /pgweb/ {
-        proxy_pass http://127.0.0.1:5050/;
+    # location /pgweb/ {
+    #     proxy_pass http://127.0.0.1:5050/;
+    #     proxy_set_header Host \$host;
+    #     proxy_set_header X-Real-IP \$remote_addr;
+    #     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    #     proxy_set_header X-Forwarded-Proto \$scheme;
+    #     proxy_redirect off;
+    # }
+
+     location /api/ {
+        proxy_pass http://127.0.0.1:$BACKEND_PORT/;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -33,14 +42,7 @@ server {
         try_files \$uri /index.html;
     }
 
-    location /api/ {
-        proxy_pass http://127.0.0.1:$BACKEND_PORT/;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_redirect off;
-    }
+   
 }
 EOF
 
